04d948945c41da9c70f3ce896270dbda2de82b86
Eugene Kim <eugene02876@gmail.com>
2024-05-01 01:39:52 +0900

    롤업

--- packages/wow-ui/rollup.config.js
+++ packages/wow-ui/rollup.config.js
@@ -1,15 +1,32 @@
-import ts from "rollup-plugin-ts";
-import resolve from "@rollup/plugin-node-resolve";
-import commonjs from "@rollup/plugin-commonjs";
+import resolve from '@rollup/plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs';
+import external from 'rollup-plugin-peer-deps-external'
+import ts from 'rollup-plugin-ts'
+import babel from '@rollup/plugin-babel';
 
-export default {
-  input: 'components/src/index.ts',
-  output: {
-    dir: 'dist'
-  },
-  plugins: [
-    ts(),
-    resolve(),
-    commonjs(),
-  ]
-};
+
+export default [{
+        input: 'src/index.ts',
+        output: {
+            dir: 'dist'
+        // name: 'index',
+        // file: pkg.main,
+        // format: 'umd'
+    },
+    external: [ 'react', 'react-is', 'react-router', 'react/jsx-runtime' ],
+    plugins: [
+        external(),
+        ts(),
+        resolve(),
+        // babel(),
+        commonjs(),
+    ],
+    // external: [
+    //     'react',
+    //     'prop-types',
+    // ],
+    // globals: {
+    //     react: "React"
+    // }
+},
+];
