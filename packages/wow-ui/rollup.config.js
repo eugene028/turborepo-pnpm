fef540259585f1c998585a7d05a3b6993d82f45a
Eugene Kim <eugene02876@gmail.com>
2024-05-01 13:56:05 +0900

    publish 준비

--- packages/wow-ui/rollup.config.js
+++ packages/wow-ui/rollup.config.js
@@ -2,31 +2,22 @@ import resolve from '@rollup/plugin-node-resolve'
 import commonjs from '@rollup/plugin-commonjs';
 import external from 'rollup-plugin-peer-deps-external'
 import ts from 'rollup-plugin-ts'
+import PeerDepsExternalPlugin from 'rollup-plugin-peer-deps-external';
 import babel from '@rollup/plugin-babel';
 
 
 export default [{
         input: 'src/index.ts',
         output: {
-            dir: 'dist'
-        // name: 'index',
-        // file: pkg.main,
-        // format: 'umd'
+            dir: 'dist',
     },
     external: [ 'react', 'react-is', 'react-router', 'react/jsx-runtime' ],
     plugins: [
+        PeerDepsExternalPlugin(),
         external(),
         ts(),
         resolve(),
-        // babel(),
         commonjs(),
     ],
-    // external: [
-    //     'react',
-    //     'prop-types',
-    // ],
-    // globals: {
-    //     react: "React"
-    // }
 },
 ];
