0300348da2901a9a8bf562f766e69a3735e6d434
Eugene Kim <eugene02876@gmail.com>
2024-05-01 15:10:30 +0900

    type 선언 고치기

--- packages/wow-ui/rollup.config.js
+++ packages/wow-ui/rollup.config.js
@@ -2,6 +2,7 @@ import resolve from '@rollup/plugin-node-resolve'
 import commonjs from '@rollup/plugin-commonjs';
 import external from 'rollup-plugin-peer-deps-external'
 import ts from 'rollup-plugin-ts'
+import typescript from "rollup-plugin-typescript2";
 import PeerDepsExternalPlugin from 'rollup-plugin-peer-deps-external';
 import babel from '@rollup/plugin-babel';
 
@@ -18,6 +19,16 @@ export default [{
         ts(),
         resolve(),
         commonjs(),
+        typescript({
+            tsconfigOverride: {
+                compilerOptions: {
+                declaration: true,
+                declarationDir: './dist'
+                }
+            },
+            rollupCommonJSResolveHack: true,
+            clean: true
+        }),
     ],
 },
 ];
